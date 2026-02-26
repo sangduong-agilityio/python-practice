8017270393a6e03e387b6850ddf8c4f4afbcfb20
Sang Duong <sang.duong@asnet.com.vn>
2026-02-26 14:41:31 +0700

    add JWT access token creation and verification

--- src/fastapi_training/practice_one/app/core/config.py
+++ src/fastapi_training/practice_one/app/core/config.py
@@ -4,7 +4,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     app_name: str = "Task Management API"
     debug: bool = True
-    secret_key: str = "supersecret"
+
+    SECRET_KEY: str = "supersecret"
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = ".env"
